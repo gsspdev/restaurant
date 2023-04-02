85964a3e2d5f728c148b955703a1089ea7986566
gsspdev <108854023+gsspdev@users.noreply.github.com>
2023-04-02 21:11:58 +0000

    init commit

---
+++ src/back_of_house/cooking.rs
@@ -0,0 +1,3 @@
+pub fn make_a_soup() {
+    pub println!("Making a soup!");
+}
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+pub use restaurant::back_of_house::cooking;
+
+// let cooking = cooking::Cooking::new();
+
+fn main () {
+    cooking::make_a_soup();
+
+}
